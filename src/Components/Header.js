af073783e48dd4cd39a805cb76a6d3932961024d
AstikSharma <astiksharma26@gmail.com>
2024-05-21 16:39:35 +0530

    Initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../Styles/Header.css';
+
+const Header = () => {
+  return (
+    <div className="navbar-header" data-view="ctaHeader" data-item-id="48826891">
+      <div className="navbar-logo">
+        <a className="navbar-envato-market" href="https://codecanyon.net/item/motion-art-for-elementor-wordpress-plugin/48826891">Envato Market</a>
+      </div>
+      <div id="js-navbar-actions" className="navbar-actions">
+        <div className="navbar-action-buy">
+          <a className="btn-3d btn-3d-primary" href="https://codecanyon.net/checkout/from_item/48826891?license=regular&amp;support=bundle_6month">Buy now</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
